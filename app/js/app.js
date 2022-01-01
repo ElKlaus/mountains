31b4961ea0d3c4d2d890bcde1c8a55b39e89f4f5
ElKlaus <klaus15rus@gmail.com>
2022-01-01 22:25:52 +0300

    Step 7: adaptive footer

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,36 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
 
 	const navBtn = document.querySelector('.nav-button');
 	const mobileNav = document.querySelector('.mobile-nav');
+	const body = document.body;
 
-	navBtn.addEventListener('click', () => {
-		mobileNav.classList.toggle('mobile-nav-active');
-		navBtn.classList.toggle('nav-button-close');
+	// Клик по кнопке
+	navBtn.addEventListener('click', function (event) {
+		event.stopPropagation();
+
+		toggleMobileNav()
 	})
 
+
+
+	// Клик по окну за пределами навигации
+	window.addEventListener('click', function () {
+		if (body.classList.contains('no-scroll')) {
+			toggleMobileNav()
+		}
+	})
+
+	// Останавливаем клик внутри открытой мобильной навигации
+	mobileNav.addEventListener('click', function (event) {
+		event.stopPropagation();
+	})
+
+	function toggleMobileNav() {
+		body.classList.toggle('no-scroll');
+		navBtn.classList.toggle('nav-button-close');
+		mobileNav.classList.toggle('mobile-nav-active');
+	}
 })
